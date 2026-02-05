d943b598f0c7937a36b6e3f5e8b62f80d92cfec0
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2026-02-05 16:46:10 +0530

    Initial commit

---
+++ websokets/src/test/java/com/sumukh/websokets/WebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumukh.websokets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
